e9ed67e21ba5215fc4265c4cc2c51ed2bc48c75c
FilipBjorkskog <filip.bjorkskog@gmail.com>
2023-09-08 08:27:15 +0300

    CookieSaver

---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+
+    if (parsedUrl.pathname === '/') {
+        fs.readFile('index.html', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
